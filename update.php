1500942c5030296119c483b42b2385fd0e1418b9
Don <evidence@384.jp>
2022-10-19 14:47:24 +0900

    rewrite the PHP code in Python

--- update.php
+++
@@ -1,8 +0,0 @@
-<?php
-ob_start();
-include 'render.php';
-$html = ob_get_contents();
-ob_end_clean();
-array_map('unlink', glob('repos/*.json'));
-array_map('unlink', glob('readme/*.txt'));
-file_put_contents(__DIR__. '/docs/index.html', $html);
